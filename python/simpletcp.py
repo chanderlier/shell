c3cc1416c0e945e88938201cbb2d4be9ba0eb8b5
chanderlier <dieser@163.com>
2020-11-20 11:24:45 +0800

    111

---
+++ python/simpletcp.py
@@ -0,0 +1,12 @@
+from socket import socket
+
+
+def main():
+    client = socket()
+    client.connect(('192.168.1.6', 6789))
+    print(client.recv(1024).decode('utf-8'))
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
