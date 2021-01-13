a4e571e04b5ff9d182b477d09aca52d9e710a3a4
chanderlier <dieser@163.com>
2021-01-13 11:23:45 +0800

    up date

--- python/clock.py
+++ python/clock.py
@@ -1,13 +1,14 @@
 from time import sleep
 
+
 class Clock(object):
-    def __init__(self,hour=0, minute=0,second=0):
+    def __init__(self, hour=0, minute=0, second=0):
         self._hour = hour
         self._minute = minute
         self._second = second
-    
+
     def run(self):
-        self._second += 1 
+        self._second += 1
         if self._second == 60:
             self._second = 0
             self._minute += 1
@@ -17,9 +18,9 @@ class Clock(object):
                 if self._hour == 24:
                     self._hour = 0
 
-
     def show(self):
         return '%02d:%02d:%02d' % (self._hour, self._minute, self._second)
+
 
 def main():
     clock = Clock(23, 45, 55)
@@ -31,4 +32,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
