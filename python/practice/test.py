2cf5819f52ca1aa2924b8b18779d1865679b154e
chanderlier <dieser@163.com>
2021-02-26 10:21:01 +0800

    update

---
+++ python/practice/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3 
+while True:
+	reply = input('Enter text:')
+	if reply == 'stop': break
+	print(reply.upper())
---
+++ python/practice/test2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+while True:
+	reply = input('Enter text:')
+	if reply == 'stop':
+		break
+	elif not reply.isdigit():
+		print('Bad!' * 8)
+	else:
+		print(int(reply) ** 2)
+print('Bye')
