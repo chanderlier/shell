48dc1495ec201d12c3cf8afd64145d10aa48b185
chanderlier <dieser@163.com>
2021-03-09 10:37:27 +0800

    update

---
+++ goland/3_5/command1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	cmd := exec.Command("ls", "-lah")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("failed to call cmd.Run(): %v", err)
+	}
+}
