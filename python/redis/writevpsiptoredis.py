53ee3a8df1e7e6dfc434fcb3d70d6e247519429d
chanderlier <dieeser@163.com>
2021-10-29 15:14:25 +0800

    update

--- python/redis/writevpsiptoredis.py
+++ python/redis/writevpsiptoredis.py
@@ -2,13 +2,17 @@
 # This file is auto-generated, don't edit it. Thanks.
 # author dieser
 # contact dieser@163.com
+# 将VPS的拨号IP写入到指定redis中
 import redis
 import subprocess
 import json
 import requests
 import time
+import socket
 
 url = 'http://www.baidu.com'
+# 获取主机名
+hostname = socket.gethostname()
 
 
 def checknetwork():
@@ -19,17 +23,9 @@ def checknetwork():
         return False
 
 
-def clearoldip():
-    with open("/root/ip.txt", 'r') as f:
-        old_ip = f.readline()
-        r = redis.Redis(host='x.x.x.x', port=63790, db=22, password='passwd')
-        print(old_ip)
-        r.srem('91vps', old_ip)
-
-
 def redial():
-    subprocess.run("/usr/sbin/pppoe-stop")
-    subprocess.run("/usr/sbin/pppoe-start")
+    subprocess.Popen("/usr/sbin/pppoe-stop")
+    subprocess.Popen("/usr/sbin/pppoe-start")
     time.sleep(1)
 
 
@@ -39,29 +35,29 @@ def getip():
     #  转成 Python 字典并赋值
     ip_detail = json.loads(r.text)
     IP = ip_detail['IP']
-    newip = 'str' + str(IP) + ':10240'
+    newip = 'auth_passwd' + str(IP) + ':10240'
     with open("/root/ip.txt", 'w') as f:
         f.write(newip)
 
 
-def writeiptoredis():
-    r = redis.Redis(host='x.x.x.x', port=63790, db=22, password='passwd')
+def writeiptoredis(expire):
+    r = redis.Redis(host='ip', port=63790, db=22, password='passwd')
     with open("/root/ip.txt", 'r') as f:
         newip = f.readline()
-    r.sadd('91vps', newip)
+    r.set('ip:vps:' + hostname, newip, ex=expire)
     print(newip)
 
 
 if __name__ == "__main__":
     flag = False
+    st = int(time.time())
     if checknetwork():
-        clearoldip()
         flag = True
     redial()
     while not checknetwork():
         print("1111")
         redial()
-    if not flag:
-        clearoldip()
+    et = int(time.time())
+    expire = 300 - (et - st)
     getip()
-    writeiptoredis()
+    writeiptoredis(expire)
